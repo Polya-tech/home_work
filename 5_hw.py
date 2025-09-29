fa242fd4e83066786f33897b6108eacf986c1a7e
Polina_NX <57795510+Polya-tech@users.noreply.github.com>
2025-09-29 21:17:42 +0300

    Update 5_hw.py

--- 5_hw.py
+++ 5_hw.py
@@ -1,6 +1,3 @@
-
-
-
 #CSS_https://flukeout.github.io/
 
 1. plate
@@ -17,3 +14,21 @@
 12. plate + apple
 13. bento ~ pickle
 14. plate > apple 
+
+
+
+#XPATH_https://topswagcode.com/xpath/
+1. plate
+2. bento
+3. //plate/apple
+4. //*
+5. //*/apple
+6. //*[@id='fancy']
+7. //plate/apple
+8. //*[@id="fancy"]/pickle
+9. //*[contains(@class,'small')]
+10. //orange[contains(@class,"small")] 
+11. //bento/orange[contains(@class,"small")] 
+12. //plate|//bento
+13. //plate/*
+14. //plate/following-sibling::apple 
