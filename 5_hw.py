8237dc72a69e54096d933e782175a90429395300
Polina_NX <57795510+Polya-tech@users.noreply.github.com>
2025-10-01 22:20:49 +0300

    Update 5_hw.py

--- 5_hw.py
+++ 5_hw.py
@@ -1,22 +1,18 @@
 from selenium import webdriver
-from selenium.common import NoSuchElementException
 from selenium.webdriver.common.by import By
+from selenium.common.exceptions import NoSuchElementException
 
-driver = webdriver.Chrome()
-
-try:
+def check_elements():
+    driver = webdriver.Chrome()
     driver.get("https://www.saucedemo.com/")
 
-    username_field = driver.find_element(By.ID, "user-name")
-    password_field = driver.find_element(By.ID, "password")
-    submit_button = driver.find_element(By.CLASS_NAME, "btn_action")
+    try:
 
+        username_field = driver.find_element(By.ID, "user-name")
+        password_field = driver.find_element(By.ID, "password")
+        submit_button = driver.find_element(By.CLASS_NAME, "btn_action")
 
-    if username_field and password_field and submit_button:
-        print("Элементы найдены")
-except NoSuchElementException:
-    print("Элементы не найдены")
-
-finally:
-    driver.quit()
-
+        if username_field and password_field and submit_button:
+            print("Элементы найдены")
+    except NoSuchElementException:
+            print("Элементы не найдены")
